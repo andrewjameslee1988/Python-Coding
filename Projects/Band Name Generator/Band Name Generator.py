5f5c032c5f99d286d7de2beb187fa6ead4465ed7
andrewjameslee1988 <90531987+andrewjameslee1988@users.noreply.github.com>
2022-10-11 11:53:13 -0500

    Create Band Name Generator.py

---
+++ Projects/Band Name Generator/Band Name Generator.py
@@ -0,0 +1,11 @@
+#Welcome to the Band Name Generator
+#Create a greeting
+print("Welcome to the Band Name Generator")
+#Ask the user for the city that they grew up in.
+city = input("What's the name of the city you grew up in?\n")
+#Ask the user for the name of their favorite food.
+food_name = input("What's your favorite food or drink?\n")
+#Ask the user for an object
+kicker = input("Name a random object.\n")
+#Combine the name of their city, food, and object and show them their band name.
+print("Your band name could be " + city + " " + food_name + " " + kicker)

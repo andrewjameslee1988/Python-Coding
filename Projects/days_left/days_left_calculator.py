14b932580b5d4461530dde4d95d5b62187f6ce94
andrewjameslee1988 <90531987+andrewjameslee1988@users.noreply.github.com>
2022-10-13 12:49:47 -0500

    Create days_left_calculator.py

---
+++ Projects/days_left/days_left_calculator.py
@@ -0,0 +1,15 @@
+#Ask for age for input
+age = input("What is your current age?")
+#Change age to an integer just in case they don't give a whole number
+age_as_int= int(age)
+#Calculate how many years left until they are 90
+years_left = 90 - age_as_int
+#Create factors for # of: days/weeks/months they have left
+days_left = years_left*365
+weeks_left = years_left*52
+months_left = years_left*12
+
+#Using an f string, pull all the factors you created into a message
+#Print message
+message= f"You have {days_left} days, {weeks_left} weeks, {months_left} months left."
+print(message)
